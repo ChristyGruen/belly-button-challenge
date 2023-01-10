e9ab3dbafc32ea9db86582e6cd9aceb9f1ce10a0
ChristyGruen <chrisgruen@yahoo.com>
2023-01-10 12:12:58 -0600

    bubble plot updated for interactive

--- static/js/app.js
+++ static/js/app.js
@@ -12,33 +12,15 @@ console.log(data.names[2]);
 
 
 let nameddl = d3.select('select');
-// nameddl.append(option.attr("value",940).text(940));
-//   data.names.forEach(nameber => nameddl.append(option.attr('value', nameber).text(nameber)));
-  
-// data.names.forEach(nameber =>{
-//   let nameddl = d3.select('select').append('option');
-//   option.value = nameber,
-//   option.text = nameber
-// });
 
-  // Object.entries(demoInfo).forEach(([key,value])=> {
-  //   bob = key,
-  //   value = value,
-  //   console.log(bob)
-  //   console.log(value)
-  //   let li = d3.select('ul').append('li')
-  //   li.text(`${bob}:${value}`)
-  // });
 //nick's original code  kept getting error app.js:34 Uncaught (in promise) ReferenceError: option is not defined at app.js:34:41
 // let myddl = d3.select('select')
 // data.names.forEach(name => myddl.append(option.attr("value", name)).text(name))
-
-
   // //nick's code with a few fixes  WORKS
-  let myddl = d3.select('select')
+  let myddl = d3.select('select');
   data.names.forEach(nameber => myddl.append('option').attr('value', nameber).text(nameber));
 
-//Chris try another way - WORKS
+//Chris try another way - WORKS - keep this code for future reference
   // Object.entries(data.names).forEach(([key,value])=> {
   //   currentValue = value;
   //   console.log(currentValue)
@@ -47,14 +29,13 @@ let nameddl = d3.select('select');
   //   bob.text(currentValue)
   // });
 
+    let nameindex = 0;
 
-    let nameindex = 0
-
-    console.log(data)
-    console.log(data['names'])
-    console.log(data['metadata'])
-    console.log(data['samples'])
-    console.log(data['samples'][nameindex].id)
+    // console.log(data)
+    // console.log(data['names'])
+    // console.log(data['metadata'])
+    // console.log(data['samples'])
+    // console.log(data['samples'][nameindex].id)
 //probably don't need to map this stuff
     // let lNames = data['names'].map(function(item) {
     //     return item;
@@ -85,85 +66,27 @@ let nameddl = d3.select('select');
 let demoInfo = data.metadata[nameindex];
     console.log(demoInfo)
 
-    // metadata940 =  [{id: 940, ethnicity: "Caucasian", gender: "F", age: 24.0, location: "Beaufort/NC", bbtype: "I", wfreq: 2.0}];
-    // // metadata940 = {"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0}]
-    // currentId = demoinfo['id'];
-    // currentEthnicity = demoinfo['ethnicity'];
-    // currentGender = demoinfo['gender'];
-    // currentAge = demoinfo['age'];
-    // currentLocation = demoinfo['location'];
-    // currentBbtype = demoinfo['bbtype'];
-    // currentwfreq = demoinfo['wfreq'];
-    
-    // console.log(demoinfo.id)
-    // console.log(demoinfo.ethnicity)
-    // console.log(demoinfo.gender)
-    // console.log(demoinfo.age)
-    // console.log(demoinfo.location)
-    // console.log(demoinfo.bbtype)
-    // console.log(demoinfo.wfreq)
-// let ldemoInfo = [data.metadata[nameindex]]
-// let pimento = [];
-// ldemoInfo.forEach(olive => pimento.push(olive[0]));
-// console.log(ldemoInfo);
-// const demoinfokeys = Object.keys(demoInfo);
-// console.log(demoinfokeys)
-// //might be able to automate the list?? 
-// for(key in demoInfo){
-//   bob = Object.keys(key)
-//   value = demoInfo[key]
-//   console.log(bob)
-//   console.log(value)
-// };
-
 Object.entries(demoInfo).forEach(([key,value])=> {
-  bob = key,
-  value = value,
-  console.log(bob)
-  console.log(value)
-  let li = d3.select('ul').append('li')
+  bob = key;
+  value = value;
+  let li = d3.select('ul').append('li');
   li.text(`${bob}:${value}`)
 });
 
-    // let li1 = d3.select('ul').append('li')
-    // li1.text(`id: ${demoInfo.id}`)
-    // let li2 = d3.select('ul').append('li')
-    // li2.text(`ethnicity: ${demoInfo.ethnicity}`)
-    // let li3 = d3.select('ul').append('li')
-    // li3.text(`gender: ${demoInfo.gender}`)
-    // let li4 = d3.select('ul').append('li')
-    // li4.text(`age: ${demoInfo.age}`)
-    // let li5 = d3.select('ul').append('li')
-    // li5.text(`location: ${demoInfo.location}`)
-    // let li6 = d3.select('ul').append('li')
-    // li6.text(`bbtype: ${demoInfo.bbtype}`)
-    // let li7 = d3.select('ul').append('li')
-    // li7.text(`wfreq: ${demoInfo.wfreq}`)
-
 //BAR CHART ;
-// let ohBoyyr = data.samples[nameindex].otu_ids.slice(0,10);//.reverse();//otu_ids
-// let ohBoyxr = [data.samples[nameindex].sample_values.slice(0,10)];//.reverse();//sample_values
-// let ohBoyzr = [data.samples[nameindex].otu_labels.slice(0,10)];//.reverse();//otu_labels
-// console.log(ohBoyyr)
-// console.log(ohBoyxr)
-// console.log(ohBoyzr)
-//need to get first 10 from data
-// let ohBoyy = [`otu_id ${data.samples[nameindex].otu_ids.slice(0,10)}`];//.reverse();//otu_ids
 let ohBoyy = data.samples[nameindex].otu_ids.slice(0,10).reverse();//otu_ids
 let ohBoyystr = []
 ohBoyy.forEach(pear => ohBoyystr.push(`OTU_${pear}`));
-console.log(ohBoyystr)
 let ohBoyx = data.samples[nameindex].sample_values.slice(0,10).reverse();//sample_values
 let ohBoyz = data.samples[nameindex].otu_labels.slice(0,10).reverse();//otu_labels
 console.log(ohBoyy)
 console.log(ohBoyx)
 console.log(ohBoyz)
-//double check reverse for the otu_labels once all 10 inputs are captured, had trouble with the reversal before.
 
 let trace1 = {
-    x: ohBoyx,//.reverse(),
-    y: ohBoyystr,//.reverse(),
-    text: ohBoyz,//.reverse(),
+    x: ohBoyx,
+    y: ohBoyystr,
+    text: ohBoyz,
     type: 'bar',
     orientation: 'h',
   };
@@ -172,30 +95,28 @@ let trace1 = {
   Plotly.newPlot("bar", data1);
 
 //BUBBLEPLOT
-// works with test data
-  bubblex =[1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342];
-  bubbley =[163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  bubblez =["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Proteobacteria;Epsilonproteobacteria;Campylobacterales;Campylobacteraceae;Campylobacter", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae;Varibaculum", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Veillonellaceae", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Peptococcaceae;Peptococcus", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae;Prevotella", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria", "Bacteria;Firmicutes", "Bacteria", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria"];
+//test data
+  // bubblex =[1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342];
+  // bubbley =[163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+  // bubblez =["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Proteobacteria;Epsilonproteobacteria;Campylobacterales;Campylobacteraceae;Campylobacter", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae;Varibaculum", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Veillonellaceae", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Peptococcaceae;Peptococcus", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae;Prevotella", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria", "Bacteria;Firmicutes", "Bacteria", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria"];
  
- //THIS DOESN'T WORK, bubblex and bubbley are both arrays of integers
-//   let bubblex = [data.samples[nameindex].otu_ids];
-//   console.log(bubblex)
-//   let bubbley = [data.samples[nameindex].sample_values];
-//   console.log(bubbley)
-//   let bubblez = [data.samples[nameindex].otu_labels];
-//   console.log(bubblez)
+  let bubblex = [data.samples[nameindex].otu_ids];
+  console.log(bubblex)
+  let bubbley = [data.samples[nameindex].sample_values];
+  console.log(bubbley)
+  let bubblez = [data.samples[nameindex].otu_labels];
+  console.log(bubblez)
   
    var trace2 = {
-      x: bubblex,
-      y: bubbley,
-      text: bubblez,
+      x: bubblex[0],
+      y: bubbley[0],
+      text: bubblez[0],
       mode: 'markers',
       marker: {
-        size: bubbley,
-        color: bubblex
+        size: bubbley[0],
+        color: bubblex[0]
       }
     };
-    
     var data2 = [trace2];
 
 
